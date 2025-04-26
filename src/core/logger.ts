f7423b26794d497bdc50753c0a87ef8c054a1cc8
Daniel Henrique <henriqueevaldo@outlook.com>
2025-04-26 12:48:00 -0300

    hotfix: loglevel toggle log in console/file

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { createLogger, format, transports } from 'winston';
-import { config } from '../config/config'; // adicionado
+import { config } from '../config/config';
 
 const logsDir = path.join(__dirname, '..', '..', 'logs');
 
@@ -14,14 +14,13 @@ const logFormat = format.printf(({ level, message, timestamp }) => {
 });
 
 const allTransports = [];
+
 if (config.enableLogs) {
-  allTransports.push(new transports.Console());
-  allTransports.push(new transports.File({ filename: path.join(logsDir, 'proxy.log') }));
+  allTransports.push(new transports.Console({ level: 'info' }));
+  allTransports.push(new transports.File({ filename: path.join(logsDir, 'proxy.log'), level: 'info' }));
 } else if (config.enableErrorLogs) {
-  allTransports.push(
-    new transports.Console({ level: 'error' }),
-    new transports.File({ filename: path.join(logsDir, 'proxy.log'), level: 'error' })
-  );
+  allTransports.push(new transports.Console({ level: 'error' }));
+  allTransports.push(new transports.File({ filename: path.join(logsDir, 'proxy.log'), level: 'error' }));
 }
 
 export const logger = createLogger({
