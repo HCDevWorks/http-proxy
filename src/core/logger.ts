16458a56363d5e57bd86f1c65fa949733a23cbfb
Daniel Henrique <henriqueevaldo@outlook.com>
2025-04-26 13:16:09 -0300

    feat: enhance logger with custom filtering and improve transport configuration

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, Logger, transports } from 'winston';
 import { config } from '../config/config';
 
 const logsDir = path.join(__dirname, '..', '..', 'logs');
@@ -13,19 +13,41 @@ const logFormat = format.printf(({ level, message, timestamp }) => {
   return `[${timestamp}] [${level.toUpperCase()}] ${message}`;
 });
 
+const customFilter = format((info) => {
+  const { level } = info;
+
+  if (config.enableLogs && !config.enableErrorLogs && level === 'error') {
+    return false;
+  }
+
+  if (!config.enableLogs && config.enableErrorLogs && level !== 'error') {
+    return false;
+  }
+
+  if (!config.enableLogs && !config.enableErrorLogs) {
+    return false;
+  }
+
+  return info;
+});
+
 const allTransports = [];
 
-if (config.enableLogs) {
-  allTransports.push(new transports.Console({ level: 'info' }));
-  allTransports.push(new transports.File({ filename: path.join(logsDir, 'proxy.log'), level: 'info' }));
-} else if (config.enableErrorLogs) {
-  allTransports.push(new transports.Console({ level: 'error' }));
-  allTransports.push(new transports.File({ filename: path.join(logsDir, 'proxy.log'), level: 'error' }));
+if (config.enableLogs || config.enableErrorLogs) {
+  allTransports.push(
+    new transports.Console(),
+    new transports.File({ filename: path.join(logsDir, 'proxy.log') })
+  );
+} else {
+  allTransports.push(
+    new transports.Stream({ stream: fs.createWriteStream('/dev/null') })
+  );
 }
 
-export const logger = createLogger({
-  level: config.enableLogs ? 'info' : (config.enableErrorLogs ? 'error' : 'silent'),
+export const logger: Logger = createLogger({
+  level: 'silly',
   format: format.combine(
+    customFilter(),
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     logFormat
   ),
